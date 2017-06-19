b0e26788063a3d77c3f12483a610d99a0faa99f1
Ryo Okada <atribecalledquest448@gmail.com>
2017-06-19 22:08:44 +0900

    add Headline class

--- scrapingmarket/items.py
+++ scrapingmarket/items.py
@@ -12,3 +12,7 @@ class ScrapingmarketItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class Headline(scrapy.Item):
+    title = scrapy.Field()
+    body = scrapy.Field()
