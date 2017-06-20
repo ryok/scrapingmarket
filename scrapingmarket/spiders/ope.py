d3d4347411e18acccf4a73f3401c50353383b552
Ryo Okada <atribecalledquest448@gmail.com>
2017-06-20 19:18:30 +0900

    [update]crawl link

--- scrapingmarket/spiders/ope.py
+++ scrapingmarket/spiders/ope.py
@@ -8,4 +8,9 @@ class OpeSpider(scrapy.Spider):
     start_urls = ['http://www3.boj.or.jp/market/jp/menu_o.htm']
 
     def parse(self, response):
-        print(response.css('ul a::attr("href")').extract())
+        # print(response.css('ul a::attr("href")').extract())
+        for url in response.css('ul a::attr("href")').re(r'stat/of\d+.htm$'):
+            yield scrapy.Request(response.urljoin(url), self.parse_opes)
+
+    def parse_opes(self.response)
+        pass
