7767ae3e7da0c09dd913df2d9ca2135206790d30
Ryo Okada <atribecalledquest448@gmail.com>
2017-06-21 17:34:07 +0900

    [update] scraping each tr tag

--- scrapingmarket/spiders/ope.py
+++ scrapingmarket/spiders/ope.py
@@ -9,14 +9,16 @@ class OpeSpider(scrapy.Spider):
     start_urls = ['http://www3.boj.or.jp/market/jp/menu_o.htm']
 
     def parse(self, response):
-        # print(response.css('ul a::attr("href")').extract())
         for url in response.css('ul a::attr("href")').re(r'stat/of\d+.htm$'):
             yield scrapy.Request(response.urljoin(url), self.parse_opes)
 
     def parse_opes(self, response):
-        """ページからオペの内容を抜き出す"""
+        """
+        ページからオペの内容を抜き出す
+        """
         item = OpeOffer()
         item['title'] = response.css('title::text').extract()
-        item['offer'] = response.css('td::text').extract()
+        for trtext in response.css('tr').extract():
+            for tdtext in response.css('td::text').extract():
+                item['offer'] = tdtext
         yield item
-
