d7737c7252dcb196ae730cb33086974beaf4fe87
Ryo Okada <atribecalledquest448@gmail.com>
2017-06-28 17:07:39 +0900

    [update] add validation pipline

--- scrapingmarket/pipelines.py
+++ scrapingmarket/pipelines.py
@@ -5,7 +5,16 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from pymongo import MongoClient
+from scrapy.exceptions import DropItem
 
+class ValidationPipline(object):
+    ""
+    itemを検証するPipline
+    ""
+    def process_item(self, item, spider):
+        if not item['title']:
+            raise DropItem('Missing title')
+        return item
 
 class ScrapingmarketPipeline(object):
     def process_item(self, item, spider):
@@ -15,7 +24,6 @@ class MongoPipeline(object):
 
     def open_spider(self, spider):
         uri = "mongodb://scraping-pool-mongo:5rq1tRCSzkr6algHaLa9FZ4aFKGNcZ6FJgq9Z182fpiSxvKspf42wOGUoHPvHaq3NUMRunAcuHO1rTxUVeYjkg==@scraping-pool-mongo.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
-        #client = pymongo.MongoClient(uri)
         self.client = MongoClient(uri)
         self.db = self.client['scraping-book']
         self.collection = self.db['items']
